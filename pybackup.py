f1de4eb57b9ff68e230de0e2f2e866ba89574b15
Fan() <18501341937@163.com>
2017-11-28 11:15:53 +0800

    null

--- pybackup.py
+++ pybackup.py
@@ -412,7 +412,7 @@ if __name__ == '__main__':
     '''
     参数解析
     '''
-    arguments = docopt(__doc__, version='pybackup 0.5')
+    arguments = docopt(__doc__, version='pybackup 0.6')
     print(arguments)
 
 
